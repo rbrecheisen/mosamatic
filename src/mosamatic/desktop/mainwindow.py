b9590952bcc69fbd0f63f3082c3f5b226a36b8c8
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2022-12-06 21:59:22 +0100

    Saving work

--- src/mosamatic/desktop/mainwindow.py
+++ src/mosamatic/desktop/mainwindow.py
@@ -7,6 +7,5 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Mosamatic Desktop')
-        button = QPushButton('Go!')
         self.setFixedSize(QSize(400, 300))
-        self.setCentralWidget(button)
+        self.setCentralWidget(QPushButton('Go!'))
