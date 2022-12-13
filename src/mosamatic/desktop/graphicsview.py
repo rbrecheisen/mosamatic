c89c3118a381ed59f94d74c5c50916cf7a5919e0
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2022-12-13 15:45:19 +0100

    Mouse cursor zooming works

--- src/mosamatic/desktop/graphicsview.py
+++ src/mosamatic/desktop/graphicsview.py
@@ -1,10 +1,54 @@
 import sys
 
 from PIL import Image, ImageQt
+from PySide6.QtCore import Qt, QPointF, QEvent
 from PySide6.QtWidgets import QApplication, QGraphicsScene, QGraphicsView
-from PySide6.QtGui import QPixmap
+from PySide6.QtGui import QPixmap, QTransform
 
 from utils import DicomImage
+
+# TODO: implement mouse cursor zooming
+# https://stackoverflow.com/questions/19113532/qgraphicsview-zooming-in-and-out-under-mouse-position-using-mouse-wheel
+
+
+class GraphicsView(QGraphicsView):
+
+    NORMAL = 0
+    ZOOM = 1
+    PAN = 2
+
+    STATUS_MESSAGES = {
+        NORMAL: 'NORMAL',
+        ZOOM: 'ZOOM',
+        PAN: 'PAN',
+    }
+
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.state = None
+        self.set_state(GraphicsView.NORMAL)
+
+    def set_state(self, state):
+        pass
+
+    def wheelEvent(self, event):
+        zoom_in_factor = 1.25
+        zoom_out_factor = 1 / zoom_in_factor
+        self.setTransformationAnchor(QGraphicsView.ViewportAnchor.NoAnchor)
+        self.setResizeAnchor(QGraphicsView.ViewportAnchor.NoAnchor)
+        old_pos = self.mapToScene(event.position().toPoint())
+        if event.angleDelta().y() > 0:
+            zoom_factor = zoom_in_factor
+        else:
+            zoom_factor = zoom_out_factor
+        self.scale(zoom_factor, zoom_factor)
+        new_pos = self.mapToScene(event.position().toPoint())
+        delta = new_pos - old_pos
+        self.translate(delta.x(), delta.y())
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_R:
+            self.setTransform(QTransform())
 
 
 def main():
@@ -16,7 +60,7 @@ def main():
     pixmap_image = ImageQt.ImageQt(Image.fromarray(pixels))
     scene_item = scene.addPixmap(QPixmap.fromImage(pixmap_image))
     scene_item.setPos(0, 0)
-    view = QGraphicsView(scene)
+    view = GraphicsView(scene)
     view.show()
     sys.exit(application.exec())
 
