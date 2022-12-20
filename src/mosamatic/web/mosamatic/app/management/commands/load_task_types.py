8cb9080470149c537660503c497fa2dc239c9dd0
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2022-12-20 12:04:58 +0100

    Saving work

--- src/mosamatic/web/mosamatic/app/management/commands/load_task_types.py
+++ src/mosamatic/web/mosamatic/app/management/commands/load_task_types.py
@@ -29,21 +29,5 @@ class Command(BaseCommand):
                     task_type.parameters = item['parameters']
                     task_type.save()
                     self.stdout.write(self.style.SUCCESS(f'Successfully updated task type {name}'))
-            # for name, data in settings.TASK_TYPES.items():
-            #     task_type = TaskTypeModel.objects.filter(name=name).first()
-            #     if task_type is None:
-            #         TaskTypeModel.objects.create(
-            #             name=name,
-            #             display_name=data['display_name'],
-            #             class_path=data['class_path'],
-            #             parameters=data['parameters'],
-            #         )
-            #         self.stdout.write(self.style.SUCCESS(f'Successfully created task type {name}'))
-            #     else:
-            #         task_type.display_name = data['display_name']
-            #         task_type.class_path = data['class_path']
-            #         task_type.parameters = data['parameters']
-            #         task_type.save()
-            #         self.stdout.write(self.style.SUCCESS(f'Successfully updated task type {name}'))
         except KeyError:
             raise CommandError('TASK_TYPES not in Django settings')
