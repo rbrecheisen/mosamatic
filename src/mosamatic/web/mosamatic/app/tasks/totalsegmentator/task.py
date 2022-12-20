4ce8e4da6b6e2cb3c44931027987b04d34fe6f0d
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2022-12-20 22:59:27 +0100

    Saving work

--- src/mosamatic/web/mosamatic/app/tasks/totalsegmentator/task.py
+++ src/mosamatic/web/mosamatic/app/tasks/totalsegmentator/task.py
@@ -15,25 +15,27 @@ class TotalSegmentatorTaskJob(TaskJob):
         return TotalSegmentatorTaskForm(self.task)
 
     def execute(self):
-        print('')
         self.task_job_begin()
         input_dataset = self.get_input_dataset(self.task.parameters['input'])
         output_dataset = self.create_output_dataset(name=self.task.parameters['output_dataset_name'])
-        fast = self.get_bool(self.task.parameters['fast'])
-        if fast:
-            fast = '--fast'
-        statistics = self.get_bool(self.task.parameters['statistics'])
-        if statistics:
-            statistics = '--statistics'
-        radiomics = self.get_bool(self.task.parameters['radiomics'])
-        if radiomics:
-            radiomics = '--radiomics'
-        files = self.get_files(input_dataset)
-        cmd = f'TotalSegmentator {statistics} {radiomics} {fast} -i {files[0].path} -o {output_dataset.data_dir}'
-        logger.info(f'Running command: {cmd}')
-        os.system(cmd)
-        for f in os.listdir(output_dataset.data_dir):
-            f_path = os.path.join(output_dataset.data_dir, f)
-            self.create_output_file(f_path, output_dataset)
-            logger.info(f'Added {f_path}')
+        # fast = self.get_bool(self.task.parameters['fast'])
+        logger.info('>>> input_dataset: {}, output_dataset_name: {}'.format(
+            self.task.parameters['input'], self.task.parameters['output_dataset_name'],
+        ))        
+        # if fast:
+        #     fast = '--fast'
+        # statistics = self.get_bool(self.task.parameters['statistics'])
+        # if statistics:
+        #     statistics = '--statistics'
+        # radiomics = self.get_bool(self.task.parameters['radiomics'])
+        # if radiomics:
+        #     radiomics = '--radiomics'
+        # files = self.get_files(input_dataset)
+        # cmd = f'TotalSegmentator {statistics} {radiomics} {fast} -i {files[0].path} -o {output_dataset.data_dir}'
+        # logger.info(f'Running command: {cmd}')
+        # os.system(cmd)
+        # for f in os.listdir(output_dataset.data_dir):
+        #     f_path = os.path.join(output_dataset.data_dir, f)
+        #     self.create_output_file(f_path, output_dataset)
+        #     logger.info(f'Added {f_path}')
         self.task_job_end()
