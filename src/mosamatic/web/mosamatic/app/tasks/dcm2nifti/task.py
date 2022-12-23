8d4900ef254a4c4b17d559d2d5a6cfc4825a14ff
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2022-12-23 13:41:09 +0100

    NIFTI converter saves meta data

--- src/mosamatic/web/mosamatic/app/tasks/dcm2nifti/task.py
+++ src/mosamatic/web/mosamatic/app/tasks/dcm2nifti/task.py
@@ -19,7 +19,7 @@ class DicomToNiftiTaskJob(TaskJob):
         input_dataset = self.get_input_dataset(self.task.parameters['input'])
         output_dataset = self.create_output_dataset(name=self.task.parameters['output_dataset_name'])
         output_file_path = os.path.join(output_dataset.data_dir, output_dataset.name + '.nii.gz')
-        d2n = dcm2nifti.Dicom2Nifti(input_dataset.data_dir, output_file_path)
+        d2n = dcm2nifti.Dicom2NiftiWithHeaderInfo(input_dataset.data_dir, output_file_path)
         d2n.execute()
         self.create_output_file(output_file_path, output_dataset)
         self.task_job_end()
