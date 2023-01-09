f7c5693fc2a8bed0128708d6735c7413e93cf66b
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2023-01-09 12:30:07 +0100

    Saving work

--- src/mosamatic/web/mosamatic/app/static/viewer/dicom.js
+++ src/mosamatic/web/mosamatic/app/static/viewer/dicom.js
@@ -1,7 +1,1 @@
-const element = document.getElementById("container");
-cornerstone.enabled(element);
-
-const imageId = "example://1";
-cornerstone.loadImage(imageId).then(function(image) {
-    cornerstone.displayImage(element, image);
-});
+import readImageFile from 'itk/readImageFile';
