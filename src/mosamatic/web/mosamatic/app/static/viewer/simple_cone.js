4cf8900c392e5865cf49670558022e54762ac648
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2023-01-09 10:16:10 +0100

    Saving work

---
+++ src/mosamatic/web/mosamatic/app/static/viewer/simple_cone.js
@@ -0,0 +1,30 @@
+console.log(document.currentScript.dataset.datasetName);
+
+const container = document.querySelector("#container");
+const renderWindow = vtk.Rendering.Core.vtkRenderWindow.newInstance();
+const renderer = vtk.Rendering.Core.vtkRenderer.newInstance();
+renderWindow.addRenderer(renderer);
+
+const openGLRenderWindow = vtk.Rendering.OpenGL.vtkRenderWindow.newInstance();
+openGLRenderWindow.setContainer(container);
+openGLRenderWindow.setSize(500, 500);
+renderWindow.addView(openGLRenderWindow);
+
+const interactor = vtk.Rendering.Core.vtkRenderWindowInteractor.newInstance();
+interactor.setView(openGLRenderWindow);
+interactor.initialize();
+interactor.bindEvents(container);
+
+const trackball = vtk.Interaction.Style.vtkInteractorStyleTrackballCamera.newInstance();
+interactor.setInteractorStyle(trackball);
+
+const cone = vtk.Filters.Sources.vtkConeSource.newInstance();
+const actor = vtk.Rendering.Core.vtkActor.newInstance();
+const mapper = vtk.Rendering.Core.vtkMapper.newInstance();
+
+actor.setMapper(mapper);
+mapper.setInputConnection(cone.getOutputPort());
+renderer.addActor(actor);
+
+renderer.resetCamera();
+renderWindow.render();
